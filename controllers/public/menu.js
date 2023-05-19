0d7297eeece40ab99d5bb30be6a9d66249a3008d
Duki <20210089@ricaldone.edu.sv>
2023-05-19 08:58:07 -0600

    creación carrito y main

--- controllers/public/menu.js
+++ controllers/public/menu.js
@@ -1,11 +1,15 @@
+//Codigo personalizado
+//constante para hacer acciones en el menu
+
 const openMenu = document.querySelector("#open-menu");
 const closeMenu = document.querySelector("#close-menu");
 const aside = document.querySelector("aside");
 
+//Evento para abrir el menú
 openMenu.addEventListener("click", () => {
     aside.classList.add("aside-visible");
 })
-
+//Evento para cerrar el menú
 closeMenu.addEventListener("click", () => {
     aside.classList.remove("aside-visible");
 })
