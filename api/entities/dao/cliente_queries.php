89210e26a87b67c1e369c9641f1c6deb8e09d472
Duki <20210089@ricaldone.edu.sv>
2023-06-01 16:47:52 -0600

    comentarios

--- api/entities/dao/cliente_queries.php
+++ api/entities/dao/cliente_queries.php
@@ -8,7 +8,7 @@ class ClientesQueries
     /*
     *   Métodos para gestionar la cuenta del clientes.
     */
-
+    // Método para realizar una verificación en la base de datos para comprobar la existencia y estado de un cliente 
     public function checkUser($usuario_cliente)
     {
         $sql = 'SELECT id_cliente, estado_cliente FROM clientes WHERE usuario_cliente = ?';
@@ -22,7 +22,7 @@ class ClientesQueries
             return false;
         }
     }
-
+    //Metódo que se utiliza para comparar una contraseña encriptada luego de un proceso.
     public function checkPassword($password)
     {
         $sql = 'SELECT clave_cliente FROM clientes WHERE id_cliente = ?';
@@ -34,7 +34,7 @@ class ClientesQueries
             return false;
         }
     }
-
+        //Metódo que se utiliza para hacer busqueda por alguna cosa
     public function searchRows($value)
     {
         $sql = 'SELECT id_cliente, nombre_cliente, apellido_cliente, dui_cliente, correo_cliente, telefono_cliente, nacimiento_cliente, direccion_cliente, clave_cliente, estado_cliente, genero, foto_cliente, usuario_cliente
@@ -45,7 +45,7 @@ class ClientesQueries
         $params = array("%$value%" , "%$value%" , "%$value%" , "%$value %" ,  "%$value%" , "%$value%" , "%$value%" , "%$value%" , "%$value%");
         return Database::getRows($sql, $params);
     }
-
+    //Metódo que se utiliza para crear una línea de la tabla clientes
     public function createRow()
     {
         $sql = 'INSERT INTO clientes(nombre_cliente, apellido_cliente, dui_cliente, correo_cliente, telefono_cliente, nacimiento_cliente, direccion_cliente, clave_cliente, estado_cliente, id_genero, foto_cliente, usuario_cliente)
@@ -53,7 +53,7 @@ class ClientesQueries
         $params = array($this->nombre_cliente, $this->apellido_cliente, $this->dui_cliente, $this->correo_cliente, $this->telefono_cliente, $this->nacimiento_cliente, $this->direccion_cliente, $this->clave_cliente, $this->estado_cliente, $this->id_genero, $this->foto_cliente, $this->usuario_cliente);
         return Database::executeRow($sql, $params);
     }
-
+        //Metódo que se utiliza para crear una cuante para los clientes
     public function crearCuenta(){
         $sql = "INSERT INTO  clientes(nombre_cliente, apellido_cliente, dui_cliente, correo_cliente, telefono_cliente, nacimiento_cliente,clave_cliente, usuario_cliente, estado_cliente)
         VALUES(?, ?, ?, ?, ?, ?, ?, ?, 'Activo')";
@@ -63,22 +63,7 @@ class ClientesQueries
 
     }
 
-    public function readAllGenero()
-    {
-        $sql = 'SELECT id_genero, genero
-        FROM generos
-        ORDER BY id_genero';
-        return Database::getRows($sql);
-    }
 
-
-    public function readAllEstadoCliente()
-    {
-        $sql = 'SELECT id_estado_cliente, estado_cliente
-        FROM estado_clientes
-        ORDER BY id_estado_cliente';
-        return Database::getRows($sql);
-    }
     /*
     * Método para traer todos los registros de la tabla usuarios
     */
@@ -113,6 +98,7 @@ class ClientesQueries
         $params = array($this->foto_cliente, $this->nombre_cliente, $this->apellido_cliente, $this->dui_cliente, $this->correo_cliente, $this->telefono_cliente, $this->nacimiento_cliente, $this->direccion_cliente, $this->id_estado_cliente, $this->id_genero, $this->usuario_cliente, $this->id);
         return Database::executeRow($sql, $params);
     }
+    //Metódo que se utiliza para eliminar un cliente
 
     public function deleteRow()
     {
